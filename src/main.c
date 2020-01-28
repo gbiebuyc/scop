f123f3dc4f746243e2e9e7264a5d8003334c3f67
gbiebuyc <gbiebuyc@users.noreply.github.com>
2020-01-28 09:49:47 +0100

    Enable linear filtering

--- src/main.c
+++ src/main.c
@@ -270,6 +270,8 @@ int main(int ac, char **av)
 	glGenTextures(1, &tex);
 	glBindTexture(GL_TEXTURE_2D, tex);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	free(data);
 	glEnable(GL_DEPTH_TEST);
